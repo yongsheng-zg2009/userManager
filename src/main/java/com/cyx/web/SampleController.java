0d9192a1f6a727aa2b9d31b01a9ec15c49664aa3
majunguang <majun521>
2017-04-13 09:59:55 +0800

    majunguang

--- src/main/java/com/cyx/web/SampleController.java
+++ src/main/java/com/cyx/web/SampleController.java
@@ -1,5 +1,7 @@
 package com.cyx.web;
 
+import com.cyx.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
@@ -7,23 +9,25 @@ import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomi
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
-@EnableAutoConfiguration
-public class SampleController implements EmbeddedServletContainerCustomizer{
+import java.util.List;
 
+@RestController
+//@EnableAutoConfiguration
+public class SampleController {
+    @Autowired
+    public UserService userService;
     @RequestMapping("/")
-    @ResponseBody
-    String home() {
-        return "Hello World!";
+    List home() {
+        return userService.userList();
     }
 
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(SampleController.class, args);
-    }
+//    public static void main(String[] args) throws Exception {
+//        SpringApplication.run(SampleController.class, args);
+//    }
 
-	@Override
-	public void customize(ConfigurableEmbeddedServletContainer container) {
-		container.setPort(8899);
-		container.setPort(8799);
-	}
+//	@Override
+//	public void customize(ConfigurableEmbeddedServletContainer container) {
+//		container.setPort(8899);
+//		container.setPort(8799);
+//	}
 }
