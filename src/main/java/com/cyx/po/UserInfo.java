4b8709e0e5d50194e45e41ce8d6d4937ff0bb5f5
majunguang <majun521>
2017-04-13 10:00:23 +0800

    majunguang

--- src/main/java/com/cyx/po/UserInfo.java
+++ src/main/java/com/cyx/po/UserInfo.java
@@ -4,6 +4,7 @@ package com.cyx.po;
  * @author yongsheng
  *
  */
+
 public class UserInfo {
 	
 	private String userName;// 用户名
@@ -11,4 +12,44 @@ public class UserInfo {
 	private String gender; // 性别
 	private Integer age;
 	private String registerTime;
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getNickName() {
+		return nickName;
+	}
+
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+
+	public String getRegisterTime() {
+		return registerTime;
+	}
+
+	public void setRegisterTime(String registerTime) {
+		this.registerTime = registerTime;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
 }
