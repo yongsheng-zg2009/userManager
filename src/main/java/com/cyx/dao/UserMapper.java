71eee23ef0c4af239f16c618a7902ff059a7544a
majunguang <majun521>
2017-04-13 10:00:33 +0800

    majunguang

---
+++ src/main/java/com/cyx/dao/UserMapper.java
@@ -0,0 +1,13 @@
+package com.cyx.dao;
+
+import com.cyx.po.UserInfo;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/4/13 0013.
+ */
+
+public interface UserMapper {
+    public List<UserInfo>  userList();
+}
