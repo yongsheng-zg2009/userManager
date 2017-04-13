49bea15f2a07d5af095a47db79275cd02ba516c7
majunguang <majun521>
2017-04-13 10:00:52 +0800

    majunguang

---
+++ src/main/java/com/cyx/Application.java
@@ -0,0 +1,20 @@
+package com.cyx;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 注册MyBatis分页插件PageHelper
+ *
+ * @author Angel(QQ:412887952)
+ * @version v.0.1
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public  static  void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
+
+
